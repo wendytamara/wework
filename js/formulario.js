31f4c30ceefeca2495d25e52cb6a557a72a05cad
wendytamara <anacarlavegam@gmail.com>
2018-08-03 16:18:22 -0500

    corrigiendo errores

--- js/formulario.js
+++ js/formulario.js
@@ -5,10 +5,10 @@
 
   $('#btnForm').on('click', function(event) {
 
-   var nameComplete = $('#names').val();
-   var email = $('#email').val();
-   var company = $('#company').val();
-   var  motivoVisit = $('#motivoVisit').val();
+    var nameComplete = $('#names').val();
+    var email = $('#email').val();
+    var company = $('#company').val();
+    var  motivoVisit = $('#motivoVisit').val();
 
     if (nameComplete.length > 3 && email.length > 5 && company.length > 3 && motivoVisit.length > 6 ) {
       event.preventDefault();
