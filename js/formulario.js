d9ad5275b40d9be71c1e5bf085c22c917dbdc97d
wendytamara <anacarlavegam@gmail.com>
2018-08-03 15:51:48 -0500

    añadiendo validacion del formulario

--- js/formulario.js
+++ js/formulario.js
@@ -1,10 +1,21 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     var elems = document.querySelectorAll('select');
-//     var instances = M.FormSelect.init(elems, options);
-//   });
-
-  // Or with jQuery
 
   $(document).ready(function(){
     $('select').formSelect();
   });
+
+  $('#btnForm').on('click', function(event) {
+
+   var nameComplete = $('#names').val();
+   var email = $('#email').val();
+   var company = $('#company').val();
+   var  motivoVisit = $('#motivoVisit').val();
+
+    if (nameComplete.length > 3 && email.length > 5 && company.length > 3 && motivoVisit.length > 6 ) {
+      event.preventDefault();
+      window.location.href = '../views/camara.html';
+      console.log(nameComplete);
+      console.log(email);
+      console.log(company);
+      console.log(motivoVisit);
+    }    
+  })
